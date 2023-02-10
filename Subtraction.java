3e9cae5aef547567c898b63dc6206824b69e73c0
Bramhaiah IT <ummadibramhaiah94@gmail.com>
2023-02-10 21:25:19 +0530

    first commit

---
+++ Subtraction.java
@@ -0,0 +1,16 @@
+public class Subtraction
+
+    {
+
+    public static void main(String[] args)
+    
+    {
+     
+       int a=5;
+       int b=555;
+       int c=a-b;
+       
+       System.out.println("Subtraction of two numbers is :"+c);
+    }
+
+    }
