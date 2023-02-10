ddb176bce674e3bfcdbaebebde50a3f443cf28e0
Bramhaiah IT <ummadibramhaiah94@gmail.com>
2023-02-10 21:29:23 +0530

    first commit

---
+++ Division.java
@@ -0,0 +1,16 @@
+public class Division
+
+    {
+
+    public static void main(String[] args)
+    
+    {
+     
+       int a=5;
+       int b=555;
+       int c=a/b;
+       
+       System.out.println("Division of two numbers is :"+c);
+    }
+
+    }
