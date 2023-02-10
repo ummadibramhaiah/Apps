69005436d7da351a85faa9829f05d0e086b28ce8
Bramhaiah IT <ummadibramhaiah94@gmail.com>
2023-02-10 21:20:19 +0530

    first commit

---
+++ Addition.java
@@ -0,0 +1,16 @@
+public class Addition
+
+    {
+
+    public static void main(String[] args)
+    
+    {
+     
+       int a=5;
+       int b=555;
+       int c=a+b;
+       
+       System.out.println("Addition of two numbers is :"+c);
+    }
+
+    }
